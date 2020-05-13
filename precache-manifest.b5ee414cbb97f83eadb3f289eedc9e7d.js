39355e9d062de4fc1c83c1787085bc19ce9cee63
lapenji <lapenji@MacBook-Pro-di-Lapenji.local>
2020-05-13 12:12:57 +0200

    Updates

---
+++ precache-manifest.b5ee414cbb97f83eadb3f289eedc9e7d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17b9ca30f44b1030ace9857fa81bf25f",
+    "url": "/conversation/index.html"
+  },
+  {
+    "revision": "262c7db5f552b15a0572",
+    "url": "/conversation/static/css/main.3c4dc871.chunk.css"
+  },
+  {
+    "revision": "02a40d79e81662702a08",
+    "url": "/conversation/static/js/2.99583a34.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/conversation/static/js/2.99583a34.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "262c7db5f552b15a0572",
+    "url": "/conversation/static/js/main.4bf51598.chunk.js"
+  },
+  {
+    "revision": "715da3b8924cf6b20d9e",
+    "url": "/conversation/static/js/runtime-main.f19b0ffc.js"
+  }
+]);
